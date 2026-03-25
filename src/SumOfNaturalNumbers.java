b0874e777a6867e4f6a34870880cbc3b6c007df7
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-03-25 11:41:45 +0530

    Completed sum of natural numbers program

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -1,21 +1,13 @@
 
 public class SumOfNaturalNumbers {
     
-    public static int NumberSum(int N) {
-        int sum = 0;
-        for (int i = 1; i <= N; i++) {
-            sum += i;
-        }
-        return sum;
-    }
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int N = sc.nextInt();
-        
-        System.out.println(NumberSum(N));
-        
+        int i,sum=0;
+        for(i=1;i<=N;i++)
+            sum+=i;
+        System.out.println(sum);
         sc.close();
     }
 }
