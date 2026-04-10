33d1cda0dcc5ed91ce0ea28d4b7b5de011a79738
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-10 20:58:58 +0530

    Update SumOfNaturalNumbers.java

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
 
 public class SumOfNaturalNumbers {
-    
+
+    // Create the method public static int NumberSum(int n)
+    // It should calculate and return the sum of first n natural numbers
+    public static int NumberSum(int n) {
+        return n * (n + 1) / 2;
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int i,sum=0;
-        for(i=1;i<=N;i++)
-            sum+=i;
-        System.out.println(sum);
-        sc.close();
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            int sum = NumberSum(n);
+            System.out.println(sum);
+        }
+        scanner.close();
     }
 }
